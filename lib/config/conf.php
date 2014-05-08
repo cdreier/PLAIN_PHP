e1a34f447afd2cf6352d0a514f52bcddf92f0fb5
Christian Dreier <dreier@weilacher.net>
2014-05-08 09:01:30 +0200

    parsing root path from index, no need to set path manually

--- lib/config/conf.php
+++ lib/config/conf.php
@@ -1,7 +1,5 @@
 <?php 
 $_CONFIG = array(
-    "PATH" => "http://plain-php.drailing.net/",
-    "LOCAL_PATH" => "http://localhost/PLAIN_PHP/",
     "AUTOLOAD_FOLDERS" => array(
 		"lib/vendor/PLAIN_PHP/",
 		"controllers/"
