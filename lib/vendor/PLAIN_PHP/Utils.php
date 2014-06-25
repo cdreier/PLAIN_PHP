58acac3715d5f25450ceb01137e3483cd96e3b8e
Christian Dreier <dreier@weilacher.net>
2014-06-25 13:45:10 +0200

    added slugify to utils

--- lib/vendor/PLAIN_PHP/Utils.php
+++ lib/vendor/PLAIN_PHP/Utils.php
@@ -34,6 +34,16 @@ class Utils  {
         }
         return $randomString;
     }
+	
+	public static function slugify($string, $delimiter = "-"){
+        setlocale(LC_ALL, 'en_US.UTF8');
+        $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+        $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+        $clean = strtolower(trim($clean, '-'));
+        $clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+    
+        return $clean;
+    }
     
 }
 
