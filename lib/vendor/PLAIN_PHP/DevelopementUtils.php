b56eafd6f845e889d156d23b3c45a60d1b48698b
Christian Dreier <dreiling@drailing.net>
2014-06-29 11:10:03 +0200

    dev menu

--- lib/vendor/PLAIN_PHP/DevelopementUtils.php
+++ lib/vendor/PLAIN_PHP/DevelopementUtils.php
@@ -6,7 +6,25 @@ class DevelopementUtils {
 	
 	public static function devMenu(){
 		if(PLAIN_PHP_DEV){
-			echo "asd";
+			?>
+			<style>
+				#plainDev{
+					width: 20px;
+					height: 20px;
+					position: fixed;
+					bottom: 0px;
+					right: 0px;
+					background-color: #33B5E5;
+				}
+				#plainDev:hover{
+					height: 100px;
+					width: 100px;
+				}
+			</style>
+			<div id="plainDev">
+				<p>[+]</p>
+			</div>
+			<?php 
 		}
 	}
 	
