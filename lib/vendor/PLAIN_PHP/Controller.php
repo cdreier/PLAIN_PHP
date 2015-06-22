97fe569b22c3abb691ae098116968bedb83c06ad
Christian Dreier <dreier.christian@googlemail.com>
2015-06-22 22:32:57 +0200

    added renderBinary function

--- lib/vendor/PLAIN_PHP/Controller.php
+++ lib/vendor/PLAIN_PHP/Controller.php
@@ -272,8 +272,50 @@ class Controller {
      */
 	public static function renderJSON($data){
 		header("Content-Type: application/json");
+        if(PLAIN_PHP_DEV){
+            exit(json_encode($data, JSON_PRETTY_PRINT));
+        }
 		exit(json_encode($data));
 	}
+
+    /**
+     * renders a binary file and exits script execution
+     *
+     * @link http://plain-php.drailing.net/index.php/Manual/controllers#controller_renderBinary
+     * @param string $path  the full path to the file you want to render
+     * @param string $name  optional param, if not set renderBinary will output the binary, if set, renderBinary adds attachment header and starts a download
+     * @param string $mime  optional param, if not set renderBinary tries to find the mime-type with help of finfo
+     */
+    public static function renderBinary($path, $name = false, $mime = false){
+
+        if(!is_file($path)){
+            throw new Exceptions\Exception("No file found at: ".$path);
+        }
+
+        //no explizit mime type is set, try to find with finfo
+        if(!$mime){
+            $mimeInfo = finfo_open(FILEINFO_MIME_TYPE);
+            $mime = finfo_file($mimeInfo, $path);
+        }
+        header('Content-type: '.$mime);
+
+        if($name !== false){
+            $info = new \SplFileInfo($path);
+
+            //no extension in path, try to set with mime type
+            if($info->getExtension() == ""){
+                list($garbage, $ext) = explode("/", $mime);
+                $name .= ".".$ext;
+            }
+
+            //name is found and not empty
+            if($name && $name != ""){
+                header("Content-Disposition: attachment; filename=$name");
+            }
+        }
+
+        exit(readfile($path));
+    }
     
     /**
      * includes the view at the place the function is called
